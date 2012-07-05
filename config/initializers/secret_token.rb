ad19e6c771c446d36e14b34d2b21e026a95687da
Davi Morais Ferreira <misterdmf@gmail.com>
2012-07-05 14:19:13 -0400

    project on the code host

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumac::Application.config.secret_token = 'c50b8d1aa898caa3d11f8e008c704555c1be376d6f074e4ae5bf991585c2f47531682419458abda0bd9c29a9edc2ead3fece1927427dcd6fa7bb2cf310d826df'
